adc17431b3ead171602f34208a32fa44f736e0b8
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-19 00:50:04 +0600

    Fetching Data in Server Components

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
-const PostsPage = () => {
+// const getPosts = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   return res.json();
+// };
+const getPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("failed to fetch posts");
+  }
+  return res.json();
+};
+
+// const getPosts = async () => {
+//   try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     return res.json();
+//   } catch (err) {
+//     throw new Error("failed to fetch data");
+//   }
+// };
+
+const PostsPage = async () => {
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const posts = await res.json();
+
+  const posts = getPosts();
   return (
     <div>
-      <h2>Posts are coming soon</h2>
+      <h2>Posts are coming soon{posts.length}</h2>
     </div>
   );
 };
